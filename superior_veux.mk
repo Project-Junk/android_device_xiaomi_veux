14f5a9d70950db0f51017b11b96d1ec519d04486
sourav2k <souravgope765@gmail.com>
2022-09-18 18:52:24 +0530

    Initial SuperiorOS Bringup
    Signed-off-by: sourav2k <souravgope765@gmail.com>

--- superior_veux.mk
+++ superior_veux.mk
@@ -12,14 +12,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
 # Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_QUICK_TAP := true
 
-PRODUCT_NAME := aosp_veux
+PRODUCT_NAME := superior_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
